6c7453e2c82b16a594c8b9ab6b3b728d8d1c2bb2
shvetsvlad96 <shvetsvlad96@gmail.com>
2023-03-06 01:15:22 +0200

    Homework 3 about coordinates

---
+++ src/HomeworkThree/PyramidCoordinates.java
@@ -0,0 +1,12 @@
+package HomeworkThree;
+
+public class PyramidCoordinates {
+    public static void main(String[] args) {
+        double latitude = 29.979150;
+        double longtitude = 31.134209;
+        char a = '\u00B0';
+        System.out.println("----PyramidCheops----");
+        System.out.println ("latitude = " + latitude + a);
+        System.out.println ("longtitude = " + longtitude + a);
+    }
+}
